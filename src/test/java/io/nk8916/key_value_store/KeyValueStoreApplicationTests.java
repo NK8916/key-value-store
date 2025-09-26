ead3971eee86deae3602a0aadf6959b414927794
Nitin Kumar <2018kumarnitin@gmail.com>
2025-09-26 09:20:21 +0530

    intial commit

---
+++ src/test/java/io/nk8916/key_value_store/KeyValueStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nk8916.key_value_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyValueStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
