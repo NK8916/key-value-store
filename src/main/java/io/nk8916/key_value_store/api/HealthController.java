b4af3295f6b0e3ea1c9d95b11b613a0ab333a0d2
Nitin Kumar <2018kumarnitin@gmail.com>
2025-09-30 09:20:01 +0530

    repo strcture setp

---
+++ src/main/java/io/nk8916/key_value_store/api/HealthController.java
@@ -0,0 +1,15 @@
+package io.nk8916.key_value_store.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public Map<String,String> health(){
+        return Map.of("status","OK");
+    }
+}
