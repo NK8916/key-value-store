b59407bb970b470e960e7072b341036a61f296a1
Nitin Kumar <2018kumarnitin@gmail.com>
2025-10-06 09:58:04 +0530

    add wal reader

--- src/main/java/io/nk8916/key_value_store/core/wal/WalReader.java
+++ src/main/java/io/nk8916/key_value_store/core/wal/WalReader.java
@@ -1,4 +1,34 @@
 package io.nk8916.key_value_store.core.wal;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WalReader {
+
+    public WalRecord[] readWalRecord() {
+        Path walPath = Paths.get(WalConstants.WAL_FILE_PATH);
+
+        if (!Files.exists(walPath)) {
+            return new WalRecord[0];
+        }
+        List<WalRecord> records = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+
+        try{
+            List<String> lines = Files.readAllLines(walPath);
+            for (String line : lines) {
+                WalRecord record = mapper.readValue(line, WalRecord.class);
+                records.add(record);
+            }
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        return records.toArray(new WalRecord[0]);
+    }
 }
